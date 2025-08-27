103bb7c4d018bea3198aed3347542124055df3c1
acensito <acenforos@gmail.com>
2025-08-27 11:46:56 +0200

    Llamada metodos cubit

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -22,11 +22,19 @@ class _CubitCounterView extends StatelessWidget {
 
     final counterState = context.watch<CounterCubit>().state;
 
+    void increaseCounterBy(BuildContext context, [int value = 1]) { //valor por defecto, 1
+      context.read<CounterCubit>().increaseBy(value);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cubit counter: ${counterState.transactionCount}'),
         actions: [
-          IconButton(onPressed: () => {}, icon: Icon(Icons.refresh_outlined)),
+          IconButton(
+            onPressed: () => {
+              context.read<CounterCubit>().reset()
+            }, 
+            icon: Icon(Icons.refresh_outlined)),
         ],
       ),
       body: Center(
@@ -43,19 +51,19 @@ class _CubitCounterView extends StatelessWidget {
           FloatingActionButton(
             heroTag: '1', //indicamos boton por defecto entre scaffolds
             child: Text('+3'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context,3),
           ),
           const SizedBox(height: 15),
           FloatingActionButton(
             heroTag: '2', //indicamos boton por defecto entre scaffolds
             child: Text('+2'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context,2),
           ),
           const SizedBox(height: 15),
           FloatingActionButton(
             heroTag: '3', //indicamos boton por defecto entre scaffolds
             child: Text('+1'),
-            onPressed: () => {},
+            onPressed: () => increaseCounterBy(context),
           ),
         ],
       ),
